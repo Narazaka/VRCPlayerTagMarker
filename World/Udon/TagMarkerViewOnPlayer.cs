b62dd46849442fc777a4325514f882691f17f7e3
Narazaka <info@narazaka.net>
2026-06-25 06:42:00 +0900

    useHeadPositionAsEyePosition

--- World/Udon/TagMarkerViewOnPlayer.cs
+++ World/Udon/TagMarkerViewOnPlayer.cs
@@ -9,13 +9,28 @@ namespace Narazaka.VRChat.TagMarker.World
     public class TagMarkerViewOnPlayer : TagMarkerStateInnerRenderer
     {
         [SerializeField] float scaleFromEyePosition = 1.1f;
+        [SerializeField] bool useHeadPositionAsEyePosition;
 
         void LateUpdate()
         {
             var player = Networking.GetOwner(gameObject);
             var pos = player.GetPosition();
-            var height = player.GetAvatarEyeHeightAsMeters();
-            transform.position = pos + Vector3.up * height * scaleFromEyePosition;
+            if (useHeadPositionAsEyePosition)
+            {
+                var headPos = player.GetBonePosition(HumanBodyBones.Head);
+                var height = headPos.y - pos.y;
+                transform.position = headPos + Vector3.up * height * (scaleFromEyePosition - 1f);
+            }
+            else
+            {
+                var height = player.GetAvatarEyeHeightAsMeters();
+                transform.position = pos + Vector3.up * height * scaleFromEyePosition;
+            }
+        }
+
+        void Reset()
+        {
+            useHeadPositionAsEyePosition = true;
         }
     }
 }
